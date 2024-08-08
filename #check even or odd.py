fcaba8bf5499291254819b4477cf7c7e84e33c2a
divyasandaveni <divyasandaveni5657@gmail.com>
2024-08-08 14:34:39 +0530

    day1

---
+++ #check even or odd.py
@@ -0,0 +1,15 @@
+#check even or odd
+
+def check(n):
+    if n%2==0:
+         return"even"
+    else:
+         return"odd"
+print(check(7))
+
+def check(n):
+     if 1&n==0:
+          return"even"
+     else:
+          return"odd"
+     print(check(2))
