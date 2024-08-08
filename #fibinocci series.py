e1bf0cfe7ce35823f4c1a7910911072ce079e657
divyasandaveni <divyasandaveni5657@gmail.com>
2024-08-08 15:36:41 +0530

    day1

---
+++ #fibinocci series.py
@@ -0,0 +1,30 @@
+#fibinocci series
+n=5
+def check(n):
+    first=0
+    second=1
+    print(first,second,end='')
+    count=2
+    while count<=n:
+        third=first+second
+        print(third,end='')
+        first=second
+        second=third
+        count+=1
+check(5)
+
+
+#fibinocci series
+n=5
+def check(n):
+    first=0
+    second=1
+    print(first,second,end='')
+    count=2
+    while count<=n:
+        third=first+second
+        print(third,end='')
+        first=second
+        second=third
+        count+=1
+check(10)
