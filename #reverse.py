909d2a562c8a00858eb560de9e1ff4d096187bbc
divyasandaveni <divyasandaveni5657@gmail.com>
2024-08-08 16:39:52 +0530

    day1

---
+++ #reverse.py
@@ -0,0 +1,11 @@
+#reverse
+
+def check(s):
+    s=s.split()
+    rev=''
+    for i in s:
+        rev=i+rev
+        print(type(rev))
+        return rev
+        s='sridevi is engineering college'
+    
