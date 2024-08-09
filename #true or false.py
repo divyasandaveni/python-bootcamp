ab985a8acd44c143ddcab7235c356ce48ace611c
divyasandaveni <divyasandaveni5657@gmail.com>
2024-08-09 12:53:38 +0530

    day2

---
+++ #true or false.py
@@ -0,0 +1,13 @@
+#true or false
+
+n=10
+n1=True#captial t there so true came
+print(n and n1)
+
+n=10
+n1='true'#single quotes lo true petina true ostadi
+print(n and n1)
+
+n=10
+n1=True
+print(n or n1)#or ochindi so
