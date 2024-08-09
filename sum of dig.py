af520f23e8e09ec2338dfa1150297fec1f97e666
divyasandaveni <divyasandaveni5657@gmail.com>
2024-08-09 13:36:30 +0530

    day2

---
+++ sum of dig.py
@@ -0,0 +1,16 @@
+#sum of digit until it becomes a single digit
+n=189543219
+#1+8+9+5+4+3+2+1+9=42->4+2=6
+
+n=123456
+f=123
+n=456
+
+n=12345 #the length is even or odd
+f=123
+s=54
+
+#even or odd
+n=678432
+t=678
+s=432
