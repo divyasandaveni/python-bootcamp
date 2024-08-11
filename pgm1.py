4ac0ca3a6c6f44dae368c6c358d72312f0bad9bf
divyasandaveni <divyasandaveni5657@gmail.com>
2024-08-11 21:49:41 +0530

    day4

---
+++ pgm1.py
@@ -0,0 +1,10 @@
+import math
+dir(math)
+print(math.cbrt(35))
+print(math.ceil(math.cbrt(35)))
+print(math.floor(math.cbrt(35)))
+#if not using cubic root
+print(math.ceil(35**.3))
+#not using cubic root
+print(math.floor(35**.3))
+
