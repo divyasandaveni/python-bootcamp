ce7edb89e82c94223ed86538228c5d4acaa0eb1e
divyasandaveni <divyasandaveni5657@gmail.com>
2024-08-12 19:09:26 +0530

    day5

---
+++ #convert time in 12 hr format.py
@@ -0,0 +1,16 @@
+#convert time in 12 hr format
+time='20:34:55'
+time=time.split(':')
+
+hrs=time[0]
+min=time[1]
+sec=time[2]
+if int(hrs)>12:
+    hrs=int(hrs)-12
+if int(min)>59:
+     hrs=int(hrs)+1 
+     min=0
+if int(sec)>59:
+     min=int(min)+1
+     sec=0
+print(str(hrs)+':'+str(min)+':'+str(sec))
