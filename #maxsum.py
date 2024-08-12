87f488580b6188f862f778a41428aa1c1205111d
divyasandaveni <divyasandaveni5657@gmail.com>
2024-08-12 19:40:46 +0530

    day5

---
+++ #maxsum.py
@@ -0,0 +1,21 @@
+#maxsum
+max_sum=0
+min_sum=0
+def  min_check(n):
+    s=str(n)
+    min= int(s[0])
+    for i in s:
+        if int(i)<min:
+            min=int(i)
+        return min
+def max_check(n):
+            s=str(n)
+            max=int(s[0])
+            for i in s:
+                  max=int(i)>max
+                  max=int(i)
+            return max
+min_sum=min_check(n1)+min_check(n2)+min_check(n3)
+max_sum=max_check(n1)+max_check(n2)+max_check(n3)
+diff=abs(min_sum-max_sum)
+print(diff)
